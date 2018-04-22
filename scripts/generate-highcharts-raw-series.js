bdf8d477365fecb3e325ff5e573eedd55f79ab6f
Aimee Barciauskas <aimee@developmentseed.org>
2018-04-22 17:15:53 -0400

    Add Kathmandu

--- scripts/generate-highcharts-raw-series.js
+++ scripts/generate-highcharts-raw-series.js
@@ -3,12 +3,23 @@ const dataFolder = './data/dontcommit/Shanghai';
 
 let data = [];
 fs.readdirSync(dataFolder).forEach(file => {
-  const dailyData = JSON.parse(fs.readFileSync(`${dataFolder}/${file}`));
-  dailyData.forEach((measurement) => {
-    const timestamp = new Date(measurement.local).getTime();
-    const value = measurement.value;
-    data.push([timestamp, value]);
-  });
+  try {
+    const dailyData = JSON.parse(fs.readFileSync(`${dataFolder}/${file}`));
+    dailyData.forEach((measurement) => {
+      const timestamp = new Date(measurement.local).getTime();
+      const value = measurement.value;
+      data.push([timestamp, value]);
+    });
+  } catch (e) {
+    console.log(`caught error: ${e}`)
+  }
 });
 
-fs.writeFileSync('./data/commit/Shanghai/highcharts-series-raw.json', JSON.stringify(data, null, 2));
+fs.writeFileSync('./data/commit/Shanghai/highcharts-series-raw-2016.json', JSON.stringify(data, null, 2));
+
+// data = fs.readFileSync('/Users/aimeebarciauskas/Downloads/highcharts-series-raw-kathmandu.json')
+// data = JSON.parse(data)
+// data = data.filter(x => x[1] !== -999)
+// data = data.sort()
+// data = data.filter((x, idx) => data[idx+1] && x[0] !== data[idx+1][0])
+// fs.writeFileSync('data/commit/kathmandu/highcharts-series-raw.json', JSON.stringify(data, null, 2))
--- scripts/request-and-clean.js
+++ scripts/request-and-clean.js
@@ -4,23 +4,28 @@ const fs = require('fs');
 const moment = require('moment');
 const request = require('request');
 const yaml = require('js-yaml');
+const { S3 } = require('aws-sdk');
+const s3 = new S3();
 
 const baseUrl = 'https://openaq-data.s3.amazonaws.com'
 const Flagger = require('openaq-quality-checks/lib/flagger');
 const config = yaml.safeLoad(fs.readFileSync('scripts/config.yml', 'utf8'));
 
-const startDate = moment('2015-12-31');
-const endDate = moment('2016-12-31');
+const startDate = moment('2016-12-31');
+const endDate = moment('2017-12-31');
 let currentDate = startDate;
-const locationsFilter = ['Shanghai'];
+const locationsFilter = ['Shanghai', 'Delhi'];
+const bucketName = 'aimeeb-datasets';
+const bucketPrefix = 'openaq/pm2.5';
 
 // If you want an exclusive end date (half-open interval)
 for (let currentDate = moment(startDate); currentDate.isBefore(endDate); currentDate.add(1, 'days')) {
   const currentDateString = currentDate.format('YYYY-MM-DD');
 
   request(`${baseUrl}/${currentDateString}.csv`, function (error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
+    if (error) console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log(`Downloaded data for ${currentDateString}`);
     const csvStr = body;
     let measurements = [];
 
@@ -31,7 +36,7 @@ for (let currentDate = moment(startDate); currentDate.isBefore(endDate); current
       .on('done',() => {
         // filter pm2.5
         measurements = measurements.filter((measurement) => {
-          return measurement.parameter === 'pm25'
+          return measurement.parameter === 'pm25';
         });
 
         // remove flagged data
@@ -49,16 +54,25 @@ for (let currentDate = moment(startDate); currentDate.isBefore(endDate); current
 
         locations.forEach((locationName) => {
           const locationData = groups[locationName];
+          // FOR S3: const directory = locationName.split(' ').join('_');
           const directory = `./data/dontcommit/${locationName.split(' ').join('_')}`;
+
           // TODO(aimee): Fix { Error: ENOENT: no such file or directory, mkdir './data/Horst_a/d_Maas-Hoogheide'
           try {
-            if (!fs.existsSync(directory)) fs.mkdirSync(directory);
+            body = JSON.stringify(locationData, null, 2);
+            // FOR S3:
+            // const keyName = `${bucketPrefix}/${locationName}/${currentDateString}.json`;
+            // params = {Bucket: bucketName, Key: keyName, Body: body};
+            // s3.putObject(params, function(err, data) {
+            //   if (err) {
+            //     console.log(err)
+            //   } else {
+            //     console.log(`Successfully uploaded data to ${keyName}.json`);
+            //   }
+            // });
 
-            fs.writeFileSync(
-              `${directory}/${currentDateString}.json`,
-              JSON.stringify(locationData, null, 2),
-              'utf-8'
-            ); 
+            if (!fs.existsSync(directory)) fs.mkdirSync(directory);
+            fs.writeFileSync(`${directory}/${currentDateString}.json`, body, 'utf-8');
           } catch (e) {
             console.log(e);
           }

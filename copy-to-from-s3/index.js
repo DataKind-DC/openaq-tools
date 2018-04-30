3b902d077f1796baae0c153d95b5abd0da76ef1f
Aimee Barciauskas <aimee@developmentseed.org>
2018-04-29 20:00:10 -0400

    Update copy function

--- copy-to-from-s3/index.js
+++ copy-to-from-s3/index.js
@@ -1,50 +1,59 @@
 const moment = require('moment');
 const request = require('request');
-const { S3 } = require('aws-sdk');
+const { S3, Lambda } = require('aws-sdk');
 const s3 = new S3();
+const lambda = new Lambda({region: 'us-east-1'});
 
 const baseUrl = 'https://openaq-data.s3.amazonaws.com'
 
-const startDate = moment(process.env.START_DATE || '2015-06-29');
-const endDate = moment(process.env.END_DATE || '2015-06-30');
-let currentDate = startDate;
 const bucketName = 'aimeeb-datasets';
 const bucketPrefix = 'openaq/source';
 
 async function handler(event, context, cb) {
-
+  const currentDate = moment(event.date || '2015-06-29');
   // If you want an exclusive end date (half-open interval)
-  for (let currentDate = moment(startDate); currentDate.isBefore(endDate); currentDate.add(1, 'days')) {
-    const currentDateString = currentDate.format('YYYY-MM-DD');
-    console.log('startDate')
-    console.log(startDate)
-    console.log('endDate')
-    console.log(endDate)
-
-    await request(`${baseUrl}/${currentDateString}.csv`, function (error, response, body) {
-      if (error) {
-        console.log('error:', error);
-        return cb(error);
-      }; // Print the error if one occurred
-      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-      console.log(`Downloaded data for ${currentDateString}`);
-      const csvStr = body;
-      const keyName = `${bucketPrefix}/${currentDateString}.csv`;
-      const params = {Bucket: bucketName, Key: keyName, Body: body};
-      s3.putObject(params, function(err, data) {
-        if (err) {
-          console.log(err)
-        } else {
-          console.log(`Successfully uploaded data to ${keyName}.json`);
-        }
-      });
+  const currentDateString = currentDate.format('YYYY-MM-DD');
+  await request(`${baseUrl}/${currentDateString}.csv`, function (error, response, body) {
+    if (error) {
+      console.log('error:', error);
+      return cb(error);
+    }; // Print the error if one occurred
+    const csvStr = body;
+    const keyName = `${bucketPrefix}/${currentDateString}.csv`;
+    const params = {Bucket: bucketName, Key: keyName, Body: body};
+    s3.putObject(params, function(err, data) {
+      if (err) cb(err);
     });
-  };
+  });
   return cb(null, 'Success'); 
 };
 
+async function invoker(event, context, cb) {
+  const startDate = moment(event.startDate || '2015-06-29');
+  const endDate = moment(event.endDate || '2015-06-30');
+
+  let currentDate = startDate;
+  for (let currentDate = moment(startDate); currentDate.isBefore(endDate); currentDate.add(1, 'days')) {
+    console.log('invoking function')
+    const currentDateString = currentDate.format('YYYY-MM-DD');  
+    const params = {
+      FunctionName: 'copyToFromS3', 
+      InvocationType: 'Event', 
+      Payload: JSON.stringify({date: currentDateString})
+    };
+    await lambda.invoke(params, (err, data) => {
+      if (err) {
+        cb(err);
+        console.log(err);
+      }
+    });
+  }
+  return cb(null, 'Success');
+}
+
 exports.handler = handler;
-// for testing
-// handler({}, {}, (err, result) => {
-//   console.log(result);
-// });
+exports.invoker = invoker;
+invoker({startDate: '2016-05-31', endDate: '2016-12-31'}, {}, (err, result) => {
+  if (err) console.log(err);
+  return result;
+});
